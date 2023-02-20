2859c3778778168af26fb3e611078bab44b76ed3
Fatima Mustafa <toomyox@gmail.com>
2023-02-20 00:00:35 -0800

    prints all possible different combinations of two digits

---
+++ 0x01-variables_if_else_while/test.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+    int i, j;
+    
+    for (i = 0; i < 10; i++) {
+        for (j = i; j < 10; j++) {
+            if (i != j) {
+                putchar(i + '0');
+                putchar(j + '0');
+                if (i != 8 || j != 9) {
+                    putchar(',');
+                    putchar(' ');
+                }
+            }
+        }
+    }
+    return 0;
+}
