22f3fb15a6c466e095cf3e8445e288afffc3030f
Fatima Mustafa <toomyox@gmail.com>
2023-02-25 12:07:35 -0800

    program that prints the numbers from 1 to 100, if num %3 = 0 print fizz, num % 5 = 0 print Buzz, if num % 3 and num % 5 = 0 print FizzBuzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+/**
+ * main - program that prints the numbers from 1 to 100
+ * multiples of three print Fizz
+ * multiples of five print Buzz
+ * multiples of both three and five print FizzBuzz
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 3 == 0 && i % 5 == 0)
+		{
+			printf("FizzBuzz");
+		}
+		if (i % 3 == 0)
+		{
+			printf("Fizz");
+		}
+		else if (i % 5 == 0)
+		{
+			printf("Buzz");
+		}
+		else
+		{
+			printf("%d", i);
+		}
+		if (i != 100)
+		{
+			printf(" ");
+		}
+	}
+	printf("\n");
+	return (0);
+}
