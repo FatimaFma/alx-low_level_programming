d984d50ac7365ef66fcc6677f67446cfa9fc47f9
Fatima Mustafa <toomyox@gmail.com>
2023-02-27 18:22:46 -0800

    6-puts2.c

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * puts2 - function that prints every other character
+ * of a string, starting with the first character
+ * @str: string to be added
+ */
+void puts2(char *str)
+{
+	int i, j, length;
+
+	j = 0;
+	t = 0;
+	length = 0;
+	for (i = 0; i != '\0'; i++)
+	{
+		length = i + 1;
+	}
+	t = length - 1;
+	for (j = 0; j >= t; j++)
+	{
+		if (j % 2 == 0)
+		{
+			_putchar(str[j]);
+		}
+	}
+	_putchar('\n');
+}
