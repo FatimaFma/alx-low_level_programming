3a61f197534bb2601dd5b66d13b6b5b7a185aed8
Fatima Mustafa <toomyox@gmail.com>
2023-03-18 20:06:41 -0700

    fixing

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -9,20 +9,21 @@
 int *array_range(int min, int max)
 {
 	int *p;
-	int i;
+	int i, size;
 
 	if (min > max)
 	{
 		return (NULL);
 	}
-	p = malloc((max - min + 1) * sizeof(*p));
+	size = max - min + 1;
+	p = malloc(size * sizeof(int));
 	if (p == NULL)
 	{
 		return (NULL);
 	}
 	for (i = 0; i <= max; i++)
 	{
-		p[i] = min;
+		p[i] = min++;
 	}
 	return (p);
 }
