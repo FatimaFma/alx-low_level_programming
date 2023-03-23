cdb91a8fcb97c16400814287aa73d9f9032c54c0
Fatima Mustafa <toomyox@gmail.com>
2023-03-23 02:39:09 -0700

    Define a new type struct dog

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,1 @@
+This dir contain 0x0E. C - Structures, typedef Task solution
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - struct for dpg
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: the owner name of gog
+ */
+typedef struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
