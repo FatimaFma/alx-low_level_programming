f3216f058c177c9f966d8e06ca1fb407ea26cb7e
Fatima Mustafa <toomyox@gmail.com>
2023-04-11 12:05:08 -0700

    fixing

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -6,23 +6,22 @@ void print_b(unsigned long int n);
  */
 void print_binary(unsigned long int n)
 {
-        if (n == 0)
-                _putchar('0');
-        else
-                print_b(n);
+	if (n == 0)
+		_putchar('0');
+	else
+		print_b(n);
 }
-
 /**
  * print_b - Prints binary representation of number
  * @n: Number to interpret as binary
  */
 void print_b(unsigned long int n)
 {
-        if (n == 0)
-                return;
-        print_b((n >> 1));
-        if ((n & 1) == 1)
-                _putchar('1');
-        if ((n & 1) == 0)
-                _putchar('0');
+	if (n == 0)
+		return;
+	print_b((n >> 1));
+	if ((n & 1) == 1)
+		_putchar('1');
+	if ((n & 1) == 0)
+		_putchar('0');
 }
