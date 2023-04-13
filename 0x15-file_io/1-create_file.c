39c3386d329eaef63aae1ada03046a5385897872
Fatima Mustafa <toomyox@gmail.com>
2023-04-13 05:07:50 -0700

    fixing

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -22,11 +22,12 @@ int create_file(const char *filename, char *text_content)
 	{
 		for (i = 0; text_content[i] != '\0'; i++)
 		{
-			r = write(fo, text_content, i);
-			if (r == -1)
-			{
-				return (-1);
-			}
+			;
+		}
+		r = write(fo, text_content, i);
+		if (r == -1)
+		{
+			return (-1);
 		}
 	}
 	close(fo);
